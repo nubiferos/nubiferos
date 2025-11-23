cb9acb0c8286e0e70d2c81f58e1282bcec8c8ea7
Jesse Toporowski <jessetoporowski@gmail.com>
2025-11-23 13:12:47 -0800

    additional changes to missing branding styles and calamares configuration

---
+++ installer/calamares/branding/nubiferos/lang/tz_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>Timezone</name>
+    <message>
+        <location filename="../timezone.qml" line="1"/>
+        <source>Select Timezone</source>
+        <translation>Select Timezone</translation>
+    </message>
+</context>
+</TS>
